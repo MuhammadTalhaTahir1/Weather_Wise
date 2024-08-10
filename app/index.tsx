c3dc05cc867a05d94f7e034fa80fa4e9e3c82647
Muhammad Talha <talhatahir596@gmail.com>
2024-08-10 05:08:02 -0700

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigation from "./navigation/AppNavigation";
+export default function Page() {
+  return (
+   <AppNavigation/>
+  );
+}
+
+const styles = StyleSheet.create({
+});
